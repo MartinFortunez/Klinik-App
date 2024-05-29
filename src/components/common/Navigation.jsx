6be062d6287e2c5d6965bfbe8290ff0cc4da7225
fredaadifardana <fredadifar@gmail.com>
2024-05-30 00:10:19 +0700

    Update Struktur

---
+++ src/components/common/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Button>Coba Bang</Button>
+    </div>
+  );
+};
+
+export default Navigation;
