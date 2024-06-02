2fd061c3e9595af3de95a25becebbfb037b140a1
fredaadifardana <fredadifar@gmail.com>
2024-06-02 21:08:35 +0700

    solve problem landing page

---
+++ src/components/common/landingpage/Facilities.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import CardFacilities from "../cards/landingpage/CardFacilities.jsx";
+import dataFacilities from "../../../data/facilities.js";
+import { Container } from "react-bootstrap";
+
+const Facilities = () => {
+  return (
+    <Container fluid>
+      <Row className="text-center">
+        <h1>Fasilitas Klinik</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Ullamcorper vulputate non in
+          lorem adipiscing tempor integer blandit commodo.
+        </p>
+      </Row>
+      {/* <Row className="position-absolute w-100 px-5"> */}
+      <Row xs={1} md={2} className="g-4">
+        {dataFacilities.map((data, index) => (
+          <Col lg={4} key={index}>
+            <CardFacilities
+              key={index}
+              imgSrc={data.img}
+              title={data.title}
+              desc={data.desc}
+            />
+          </Col>
+        ))}
+      </Row>
+      {/* </Row> */}
+    </Container>
+  );
+};
+
+export default Facilities;
--- src/components/specific/LandingPage.jsx
+++ src/components/specific/LandingPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import FeedBack from "../common/FeedBack";
-import Footer from "../common/Footer";
-import Doctor from "../common/Doctor";
+import FeedBack from "../common/landingpage/FeedBack";
+import Footer from "../common/landingpage/Footer";
+import Doctor from "../common/landingpage/Doctor";
 import Navigation from "../common/landingpage/Navigation";
 import Hero from "../common/landingpage/Hero";
 import About from "../common/landingpage/About";
 import "../../sass/custom.scss";
 import Superiority from "../common/landingpage/Superiority";
+import Facilities from "../common/landingpage/Facilities";
 
 const LandingPage = () => {
   return (
@@ -15,8 +16,9 @@ const LandingPage = () => {
       <Hero />
       <Superiority />
       <About />
-    <Doctor />
-          <FeedBack />
+      <Doctor />
+      <Facilities />
+      <FeedBack />
       <Footer />
     </div>
   );
