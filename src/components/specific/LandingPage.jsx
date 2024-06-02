28b121abca21a0f6636b500d7b2b01688b1228d7
alvinliansyah <alvinamrilliansyah@gmail.com>
2024-06-02 20:05:23 +0700

    Section Feedback dan Footer pada Landing Page

--- src/components/specific/LandingPage.jsx
+++ src/components/specific/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navigation from "../common/Navigation";
+import FeedBack from "../common/FeedBack";
+import Footer from "../common/Footer";
 
 const LandingPage = () => {
   return (
     <div>
       <Navigation />
+      <FeedBack />
+      <Footer />
     </div>
   );
 };
