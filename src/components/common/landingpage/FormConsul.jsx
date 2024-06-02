68a7a23fba774f7ae4323d9baa0f7f7ad26283ca
zittawardani <zittawardani@gmail.com>
2024-06-02 22:21:32 +0700

    Form Konsul update

--- src/components/common/landingpage/FormConsul.jsx
+++ src/components/common/landingpage/FormConsul.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { Formik } from "formik";
+import React, { useState } from "react";
+import { Modal, Form, Button, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
 
 function FormConsul(props) {
   return (
@@ -17,16 +18,192 @@ function FormConsul(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+      <Formik
+          // validationSchema={validationSchema}
+          // onSubmit={console.log("submit")}
+          // initialValues={{
+          //   username: "",
+          //   password: "",
+          // }}
+        >
+          {({ handleSubmit, values, touched, errors, handleChange }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>NIK</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Nama</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Alamat</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Jenis Kelamin</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Tanggal Lahir</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Golongan Darah</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>No Hp / WhatsApp</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Dokter</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Spesialis</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="validationUsername" className="mb-3">
+              <Form.Label>Sesi</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  // value={values.username}
+                  // onChange={handleChange}
+                  // isValid={touched.username && !errors.username}
+                  // isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {/* {errors.username} */}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Col>
+                  <Button
+                    variant="secondary"
+                    type="button"
+                    className="w-100 bg-transparent border-0"
+                    onClick={props.onHide}
+                  >
+                    Batal
+                  </Button>
+                </Col>
+                <Col>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    className="w-100 text-light"
+                    // onClick={handleLogin}
+                  >
+                    Login
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          )}
+        </Formik>
       </Modal.Body>
-      <Modal.Footer className="border-0">
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
     </Modal>
   );
 }
