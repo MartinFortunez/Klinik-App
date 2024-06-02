06366af7f321591d93db99c16a763649fc1ea9ea
fredaadifardana <fredadifar@gmail.com>
2024-06-02 22:38:48 +0700

    update form consul

--- src/components/common/landingpage/FormConsul.jsx
+++ src/components/common/landingpage/FormConsul.jsx
@@ -18,165 +18,171 @@ function FormConsul(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      <Formik
-          // validationSchema={validationSchema}
-          // onSubmit={console.log("submit")}
-          // initialValues={{
-          //   username: "",
-          //   password: "",
-          // }}
+        <Formik
+        // validationSchema={validationSchema}
+        // onSubmit={console.log("submit")}
+        // initialValues={{
+        //   username: "",
+        //   password: "",
+        // }}
         >
           {({ handleSubmit, values, touched, errors, handleChange }) => (
-            <Form noValidate onSubmit={handleSubmit}>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>NIK</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
+            <Form
+              noValidate
+              onSubmit={handleSubmit}
+              className="d-flex flex-column gap-2"
+            >
+              <Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>NIK</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>Nama</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>Alamat</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>Jenis Kelamin</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>Tanggal Lahir</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>Golongan Darah</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationUsername" className="mb-3">
+                  <Form.Label>No Hp / WhatsApp</Form.Label>
+                  <Form.Control
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    // value={values.username}
+                    // onChange={handleChange}
+                    // isValid={touched.username && !errors.username}
+                    // isInvalid={touched.username && !!errors.username}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {/* {errors.username} */}
+                  </Form.Control.Feedback>
+                </Form.Group>
               </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Nama</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Alamat</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Jenis Kelamin</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Tanggal Lahir</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Golongan Darah</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>No Hp / WhatsApp</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Dokter</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Spesialis</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationUsername" className="mb-3">
-              <Form.Label>Sesi</Form.Label>
-                <Form.Control
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  // value={values.username}
-                  // onChange={handleChange}
-                  // isValid={touched.username && !errors.username}
-                  // isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {/* {errors.username} */}
-                </Form.Control.Feedback>
+              <Form.Group>
+                <Form.Label className="fw-semibold">Pilih Dokter</Form.Label>
+                <Row>
+                  <Form.Group
+                    as={Col}
+                    controlId="validationUsername"
+                    className="mb-3"
+                  >
+                    <Form.Label>Spesialis</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                      <option>Pilih Spesialis</option>
+                      <option value="1">Spesialis 1</option>
+                      <option value="2">Spesialis 2</option>
+                      <option value="3">Spesialis 3</option>
+                    </Form.Select>
+                  </Form.Group>
+                  <Form.Group
+                    as={Col}
+                    controlId="validationUsername"
+                    className="mb-3"
+                  >
+                    <Form.Label>Dokter</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                      <option>Pilih Dokter</option>
+                      <option value="1">Dokter 1</option>
+                      <option value="2">Dokter 2</option>
+                      <option value="3">Dokter 3</option>
+                    </Form.Select>
+                  </Form.Group>
+
+                  <Form.Group
+                    as={Col}
+                    controlId="validationUsername"
+                    className="mb-3"
+                  >
+                    <Form.Label>Sesi</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                      <option>Pilih Sesi</option>
+                      <option value="1">Sesi 1</option>
+                      <option value="2">Sesi 2</option>
+                      <option value="3">Sesi 3</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Row>
               </Form.Group>
               <Form.Group as={Row}>
                 <Col>
