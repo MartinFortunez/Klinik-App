866139c2c460322afde8115e6d06d0d3283b54e2
fredaadifardana <fredadifar@gmail.com>
2024-06-10 15:51:12 +0700

    fect data to landing page faciities

--- src/components/common/cards/landingpage/CardFacilities.jsx
+++ src/components/common/cards/landingpage/CardFacilities.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Card, Image } from "react-bootstrap";
 
-const CardFacilities = ({ imgSrc, title, desc }) => {
-    return (
-        <Card className="d-flex flex-column align-items-center">
-            <Image src={imgSrc} fluid/>
-            <Card.Body>
-                <Card.Title className="fw-bold text-primary">{title}</Card.Title>
-                <Card.Text>{desc}</Card.Text>
-            </Card.Body>
-        </Card>
-    );
+const CardFacilities = ({ data }) => {
+  const { foto_fasilitas, judul, deskripsi } = data;
+
+  return (
+    <Card className="d-flex flex-column align-items-center">
+      <Image src={`data:image/jpeg;base64,${foto_fasilitas}`} fluid />
+      <Card.Body>
+        <Card.Title className="fw-bold text-primary">{judul}</Card.Title>
+        <Card.Text>{deskripsi}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default CardFacilities;
