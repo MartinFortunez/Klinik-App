3db727e54ee0a0f176d468a6cf0887ce47236203
MartinFortunez <martinkarunia2003.com>
2024-06-14 09:27:08 +0700

    perbaikan typo untuk riwayat dan reminder send whatsapp

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -137,14 +137,14 @@ router.delete("/dashboard/jadwal-konsultasi/reject/:id", jadwalKonsulController.
 
 // start reminder routes
 router.get("/dashboard/reminder", reminderController.getAllReminder);
-router.post('/reminder/send-whatsapp/:id', reminderController.sendWhatsAppMessageReminder);
+router.post('/dashboard/reminder/send-whatsapp/:id', reminderController.sendWhatsAppMessageReminder);
 // end reminder routes
 
 
 // start riwayat routes
 router.get("/dashboard/riwayat/search", riwayatController.getAllRiwayatCancel);
 router.get("/dashboard/riwayat", riwayatController.getAllRiwayatCancel);
-router.post('/riwayat/send-whatsapp/:id', riwayatController.sendWhatsAppMessageRiwayat);
+router.post('/dashboard/riwayat/send-whatsapp/:id', riwayatController.sendWhatsAppMessageRiwayat);
 // end riwayat routes
 
 
