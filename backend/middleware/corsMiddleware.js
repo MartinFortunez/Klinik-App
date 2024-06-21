d3a3058f252ac8d8dd3e3db9b020c241a18c45ba
trainerfe3gamelab <162530620+trainerfe3gamelab@users.noreply.github.com>
2024-06-21 10:44:17 +0700

    Update corsMiddleware.js

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,18 +1,7 @@
 const cors = require("cors");
 
-const allowedOrigins = [
-  "https://fe-msib-6-klinik-app-01.educalab.id",
-  // add other allowed origins here
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://fe-msib-6-klinik-app-01.educalab.id/",
   optionsSuccessStatus: 200,
   credentials: true,
 };
