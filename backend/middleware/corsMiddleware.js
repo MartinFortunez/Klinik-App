91d44946c2694248ff75b838bd36fb5c49835088
fredaadifardana <fredadifar@gmail.com>
2024-06-21 08:40:10 +0700

    update middleware

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:3001", // Ganti dengan URL frontend Anda
+  origin: "https://fe-msib-6-klinik-app-01.educalab.id/", // Ganti dengan URL frontend Anda
   optionsSuccessStatus: 200, // Beberapa browser lama (IE11, berbagai SmartTV) memerlukan ini
   credentials: true, // Izinkan kredensial (seperti cookie)
 };
