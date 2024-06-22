8cf63c4738470a9c8327649b35f285fcac56fd15
fredaadifardana <fredadifar@gmail.com>
2024-06-22 18:15:28 +0700

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -15,18 +15,26 @@
 
 // module.exports = connection;
 
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "mysql",
+//   }
+// );
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize("sql12715416", "sql12715416", "mJMqGbDBys", {
+  host: "sql12.freemysqlhosting.net",
+  port: 3306,
+  dialect: "mysql",
+});
 
 const connectDB = async () => {
   try {
