74474717035ae9d49c2f96d7e98755ae2b91b6bd
fredaadifardana <fredadifar@gmail.com>
2024-06-22 11:16:01 +0700

    update again

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://fe-msib-6-klinik-app-01.educalab.id",
+  origin: true,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders:
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
